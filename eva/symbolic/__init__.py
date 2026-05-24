c03a0e4f24591d8fc9b45a9a188480c06e2a47fa
BlackCatSpb <black.cat.spb.78@gmail.com>
2026-05-25 00:32:46 +0300

    Cleanup: archive 14 orphaned modules, remove dupes, clean __init__

--- eva/symbolic/__init__.py
+++ eva/symbolic/__init__.py
@@ -1,21 +1,14 @@
 """
-EVA Symbolic — символьное ядро EVA (полное).
+EVA Symbolic — символьное ядро EVA.
 
-Многообразие символьных сборок + дуальный движок + генерация + мета-уровень.
-21 модуль.
+Активные модули (используются train_* скриптами).
+Архивированные модули — в eva/archive/.
 """
 
 from .char_vocab import CharacterVocab
 from .potential_field import PotentialField
 from .potential_dynamics import PotentialDynamics
-from .assembly_graph import AssemblyState, AssemblyEdge, AssemblyNode
-from .assembly_grammar import AssemblyGrammar, AssemblyPattern
-from .semantic_closure import SemanticClosureChecker
-from .continuation_propagator import ContinuationPropagator
-from .logic_bridge import LogicBridge, TransformRule, TransformOp
-from .assembly_validator import AssemblyValidator
-from .assembly_explorer import AssemblyExplorer, ExplorationResult
-from .sleep_mode_symbolic import SleepModeSymbolic
+from .potential_function import PotentialFunction
 from .topological_field import TopologicalField, ManifoldPoint, LocalChart
 from .natural_clusterer import NaturalClusterer, Domain
 from .geodesic_navigator import GeodesicNavigator, TangentSpace, TangentVector, GeodesicPath, TangentDirection
@@ -24,48 +17,11 @@ from .contradiction_filter import SymbolicContradictionFilter, ForbiddenConnecti
 from .concept_miner import SymbolicConceptMiner, Concept
 from .symbolic_generator import SymbolicGenerator
 from .conditional_binding import TemporalConditionalBinding
-from .assembly_template_bank import AssemblyTemplateBank, AssemblyTemplate
-from .intrinsic_reward import IntrinsicReward, MetaPatterns, HierarchicalCompressor
-from .attention_feedback import AttentionFeedback
-from .advanced_modules import UncertaintyQuantifier, ActiveInference, CausalDiscovery
+from .sleep_mode_symbolic import SleepModeSymbolic
 from .advanced_methods import NGramContext, DynamicVocab, PatternToConcept, MultiLevelPredictor
 from .hierarchical_layer import WordToken, WordDiscovery, MultiLayerManifold, HierarchicalPredictor, LogicCompiler
 from .word_level import WordBoundaryDetector, GrammaticalRoleDiscovery, SemanticClustering, WordLevelGenerator, SelfConsistencyCheck
 from .knowledge_base import KnowledgeDomain, KnowledgeBase, CrossDomainNavigator, IntelligentContextRouter
 from .library import DomainAutoNamer, DomainIndex, CatalogEntry, LibrarianMap, LibraryStats, LibraryManager
 from .contemplation import LogicGuard, ContemplationLoop
-from .manifold_attention import ManifoldAttention, MultiScaleAttentionStack, CoordinateProjector
-from .parallel_trainer import ParallelSymbolicTrainer
 from .potential_trainer import PotentialTrainer
-
-__all__ = [
-    "CharacterVocab", "PotentialField", "PotentialDynamics",
-    "AssemblyState", "AssemblyEdge", "AssemblyNode",
-    "AssemblyGrammar", "AssemblyPattern",
-    "SemanticClosureChecker", "ContinuationPropagator",
-    "LogicBridge", "TransformRule", "TransformOp",
-    "AssemblyValidator", "AssemblyExplorer", "ExplorationResult",
-    "SleepModeSymbolic",
-    "TopologicalField", "ManifoldPoint", "LocalChart",
-    "NaturalClusterer", "Domain",
-    "GeodesicNavigator", "TangentSpace", "TangentVector", "GeodesicPath", "TangentDirection",
-    "CurvatureAnalyzer",
-    "SymbolicContradictionFilter", "ForbiddenConnection", "ContradictionType",
-    "SymbolicConceptMiner", "Concept",
-    "SymbolicGenerator",
-    "TemporalConditionalBinding",
-    "AssemblyTemplateBank", "AssemblyTemplate",
-    "IntrinsicReward", "MetaPatterns", "HierarchicalCompressor",
-    "AttentionFeedback",
-    "UncertaintyQuantifier", "ActiveInference", "CausalDiscovery",
-    "NGramContext", "DynamicVocab", "PatternToConcept", "MultiLevelPredictor",
-    "WordToken", "WordDiscovery", "MultiLayerManifold", "HierarchicalPredictor", "LogicCompiler",
-    "WordBoundaryDetector", "GrammaticalRoleDiscovery", "SemanticClustering",
-    "WordLevelGenerator", "SelfConsistencyCheck",
-    "KnowledgeDomain", "KnowledgeBase", "CrossDomainNavigator", "IntelligentContextRouter",
-    "DomainAutoNamer", "DomainIndex", "CatalogEntry", "LibrarianMap", "LibraryStats", "LibraryManager",
-    "LogicGuard", "ContemplationLoop",
-    "ManifoldAttention", "MultiScaleAttentionStack", "CoordinateProjector",
-    "ParallelSymbolicTrainer",
-    "PotentialTrainer",
-]
